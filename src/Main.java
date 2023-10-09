b167353edef86d4e293dca28f7a68187dd995e73
GaraevIM <garaevildarm@gmail.com>
2023-10-09 20:29:12 +0400

    IC

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Program");
+
+        int[] numbers = new int[7];
+        for (int w = 0; w < numbers.length; w++) {
+            numbers[w] = w * 10;
+            System.out.print(numbers[w] + " ");
+        }
+
+
+
+
+        /*int salary = 65789;
+        int total = 0;
+        for (int k = 0; total < 1_000_000; k++) {
+            total = total + total / 100;
+            total = total + salary;
+            if (k % 5 == 0) {
+            System.out.println(total);
+            */
+
+        /*int age = 25;
+        if (age >= 18 && age < 21) {
+            System.out.println("You can driving car, but can`t drink alcohol");
+            if (age >= 21) {
+                System.out.println("You can driving car and drink alcohol");
+            }} else
+            System.out.println("Fuck off");
+        */
     }
 }
